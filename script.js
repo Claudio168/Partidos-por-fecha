d8687f9b723f75ac601b1c617b607e6c1cd3a15f
Claudio <claudiogonzalez0712@gmail.com>
2023-04-14 00:08:43 -0400

    Se termina el localstorage y se le da estilos a la datatable

--- script.js
+++ script.js
@@ -18,58 +18,60 @@ datepicker.addEventListener("change", function () {
 
 const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-function fetchData() {
-  return fetch(`https://v3.football.api-sports.io/fixtures?date=${fecha}&timezone=${timeZone}`,
-    {
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": "v3.football.api-sports.io",
-        "x-rapidapi-key": "22240ee711b7e2faa6b80fa55809e7db",
+function functionAll() {
+  url = `https://v3.football.api-sports.io/fixtures?date=${fecha}&timezone=${timeZone}`;
+
+  function getData() {
+    return new Promise((resolve, reject) => {
+      const storedData = localStorage.getItem("allMatch");
+
+      if (storedData) {
+        const parsed_json = JSON.parse(storedData);
+
+        if (fecha === parsed_json.fecha) {
+          const parsedData = JSON.parse(storedData);
+          console.log("Datos desde localstorage");
+          resolve(parsed_json.data);
+        } else {
+          fetchData()
+            .then((data) => {
+              const newData = { fecha: fecha, data };
+              localStorage.setItem("allMatch", JSON.stringify(newData));
+              resolve(data);
+            })
+            .catch((err) => reject(err));
+        }
+      } else {
+        fetchData()
+          .then((data) => {
+            const newData = { fecha: fecha, data };
+            localStorage.setItem("allMatch", JSON.stringify(newData));
+            resolve(data);
+          })
+          .catch((err) => reject(err));
       }
-    }
-  )
-  .then((response) => response.json())
-  .then((data) => {
-    console.log("Datos de la API:", data);
-    return data.response;
-  });
-}
-
-function getData() {
-  return new Promise((resolve, reject) => {
-    const data = localStorage.getItem('todosPartidos'+fecha);
-    if (data) {
-      console.log("Datos del localStorage:", data);
-      resolve(JSON.parse(data));
-    } else {
-      fetchData()
-        .then((data) => {
-          localStorage.setItem('todosPartidos'+fecha, JSON.stringify(data));
-          resolve(data);
-        })
-        .catch((err) => reject(err));
-    }
-  });
-}
-
-function mostrarDatos() {
-  // Si ya existe una tabla creada, destruirla
-  if ($.fn.DataTable.isDataTable("#tablaResul")) {
-    $("#tablaResul").DataTable().clear().destroy();
+    });
   }
-  
-  getData()
-    .then((data) => {
-      const partidos = document.getElementById('Partidos');
-      partidos.innerHTML = '';
-      data.forEach((element) => {
-        const fecha = new Date(element.fixture.date);
-        const fechaCorta = fecha.toLocaleDateString('es-CL');
-        const unixTime = element.fixture.timestamp;
-        const fechaHora = new Date(unixTime * 1000);
-        const hora = fechaHora.toLocaleTimeString();
-        const tr = document.createElement('tr');
-        tr.innerHTML = `
+
+  function mostrarDatos() {
+    // Si ya existe una tabla creada, destruirla
+    if ($.fn.DataTable.isDataTable("#tablaResul")) {
+      $("#tablaResul").DataTable().clear().destroy();
+    }
+    const loader = document.querySelector(".loader");
+    loader.style.display = "block";
+    getData()
+      .then((data) => {
+        const partidos = document.getElementById("Partidos");
+        partidos.innerHTML = "";
+        data.forEach((element) => {
+          const fecha = new Date(element.fixture.date);
+          const fechaCorta = fecha.toLocaleDateString("es-CL");
+          const unixTime = element.fixture.timestamp;
+          const fechaHora = new Date(unixTime * 1000);
+          const hora = fechaHora.toLocaleTimeString();
+          const tr = document.createElement("tr");
+          tr.innerHTML = `
           <td>${fechaCorta}</td>
           <td>${hora}</td>
           <td>${element.league.country}</td>
@@ -77,37 +79,434 @@ function mostrarDatos() {
           <td>${element.league.round}</td>
           <td>${element.league.season}</td>
           <td>
-            <img src="${element.teams.home.logo}" alt="Imagen del equipo local" class="centrada" />
+            <img src="${
+              element.teams.home.logo
+            }" alt="Imagen del equipo local" class="centrada" />
             ${element.teams.home.name}
           </td>
           <td>
-            <img src="${element.teams.away.logo}" alt="Imagen del equipo visitante" class="centrada" />
+            <img src="${
+              element.teams.away.logo
+            }" alt="Imagen del equipo visitante" class="centrada" />
             ${element.teams.away.name} 
           </td>
-          <td>${element.score.halftime.home !== null ? element.score.halftime.home : '-'} : ${element.score.halftime.away !== null ? element.score.halftime.away : '-'}</td>
-          <td>${element.score.fulltime.home !== null ? element.score.fulltime.home : '-'} : ${element.score.fulltime.away !== null ? element.score.fulltime.away : '-'}</td>
-          <td><a href="pronosticos.html?fixture=${element.fixture.id}">Ver</a></td>
-          <td><a href="odds.html?fixture=${element.fixture.id}&home=${element.teams.home.name}&away=${element.teams.away.name}">Cuota</a></td>
+          <td>${
+            element.score.halftime.home !== null
+              ? element.score.halftime.home
+              : "-"
+          } : ${
+            element.score.halftime.away !== null
+              ? element.score.halftime.away
+              : "-"
+          }</td>
+          <td>${
+            element.score.fulltime.home !== null
+              ? element.score.fulltime.home
+              : "-"
+          } : ${
+            element.score.fulltime.away !== null
+              ? element.score.fulltime.away
+              : "-"
+          }</td>
+          <td><a href="pronosticos.html?fixture=${
+            element.fixture.id
+          }">Ver</a></td>
+          <td><a href="odds.html?fixture=${element.fixture.id}&home=${
+            element.teams.home.name
+          }&away=${element.teams.away.name}">Cuota</a></td>
         `;
-        partidos.appendChild(tr);
-      });
-
-      $(document).ready(function () {
-        $('#tablaResul').DataTable({
-          responsive: true,
-          language: {
-            url: '//cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json',
-          },
-          bInfo: false,
+          partidos.appendChild(tr);
         });
-      });
 
-      loader.style.display = "none";
-    })
-    .catch((err) => {
-      console.log(err);
-     
+        $(document).ready(function () {
+          $("#tablaResul").DataTable({
+            responsive: true,
+            language: {
+              url: "//cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json",
+            },
+            bInfo: false,
+          });
+        });
+
+        loader.style.display = "none";
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  mostrarDatos();
+}
+
+function functionLive() {
+  url = `https://v3.football.api-sports.io/fixtures?live=all&timezone=${timeZone}`;
+  function getData() {
+    return new Promise((resolve, reject) => {
+      const storedData = localStorage.getItem("LiveMatch");
+
+      if (storedData) {
+        const parsed_json = JSON.parse(storedData);
+        const tiempo_actual = Date.now();
+        if (tiempo_actual - parsed_json.fecha <= 60000) {
+          console.log("Datos desde Localstorage");
+          resolve(parsed_json.data);
+        } else {
+          fetchData()
+            .then((data) => {
+              const tiempo_inicio = Date.now();
+              const newData = { fecha: tiempo_inicio, data };
+              localStorage.setItem("LiveMatch", JSON.stringify(newData));
+              resolve(data);
+            })
+            .catch((err) => reject(err));
+        }
+      } else {
+        fetchData()
+          .then((data) => {
+            const tiempo_inicio = Date.now();
+            const newData = { fecha: tiempo_inicio, data };
+            localStorage.setItem("LiveMatch", JSON.stringify(newData));
+            resolve(data.response);
+          })
+          .catch((err) => reject(err));
+      }
+    });
+  }
+
+  function mostrarDatos() {
+    // Si ya existe una tabla creada, destruirla
+    if ($.fn.DataTable.isDataTable("#tablaResul")) {
+      $("#tablaResul").DataTable().clear().destroy();
+    }
+    const loader = document.querySelector(".loader");
+    loader.style.display = "block";
+    getData()
+      .then((data) => {
+        const partidos = document.getElementById("Partidos");
+        partidos.innerHTML = "";
+        data.forEach((element) => {
+          const fecha = new Date(element.fixture.date);
+          const fechaCorta = fecha.toLocaleDateString("es-CL");
+          const unixTime = element.fixture.timestamp;
+          const fechaHora = new Date(unixTime * 1000);
+          const hora = fechaHora.toLocaleTimeString();
+          const tr = document.createElement("tr");
+          tr.innerHTML = `
+          <td>${fechaCorta}</td>
+          <td>${hora}</td>
+          <td>${element.league.country}</td>
+          <td>${element.league.name}</td>
+          <td>${element.league.round}</td>
+          <td>${element.league.season}</td>
+          <td>
+            <img src="${
+              element.teams.home.logo
+            }" alt="Imagen del equipo local" class="centrada" />
+            ${element.teams.home.name}
+          </td>
+          <td>
+            <img src="${
+              element.teams.away.logo
+            }" alt="Imagen del equipo visitante" class="centrada" />
+            ${element.teams.away.name} 
+          </td>
+          <td>${
+            element.score.halftime.home !== null
+              ? element.score.halftime.home
+              : "-"
+          } : ${
+            element.score.halftime.away !== null
+              ? element.score.halftime.away
+              : "-"
+          }</td>
+          <td>${
+            element.score.fulltime.home !== null
+              ? element.score.fulltime.home
+              : "-"
+          } : ${
+            element.score.fulltime.away !== null
+              ? element.score.fulltime.away
+              : "-"
+          }</td>
+          <td><a href="pronosticos.html?fixture=${
+            element.fixture.id
+          }">Ver</a></td>
+          <td><a href="odds.html?fixture=${element.fixture.id}&home=${
+            element.teams.home.name
+          }&away=${element.teams.away.name}">Cuota</a></td>
+        `;
+          partidos.appendChild(tr);
+        });
+
+        $(document).ready(function () {
+          $("#tablaResul").DataTable({
+            responsive: true,
+            language: {
+              url: "//cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json",
+            },
+            bInfo: false,
+          });
+        });
+
+        loader.style.display = "none";
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  mostrarDatos();
+}
+
+function functionFinished() {
+  url = `https://v3.football.api-sports.io/fixtures?status=FT&date=${fecha}&timezone=${timeZone}`;
+  function getData() {
+    return new Promise((resolve, reject) => {
+      const storedData = localStorage.getItem("FinishedMatch");
+
+      if (storedData) {
+        const parsed_json = JSON.parse(storedData);
+        const tiempo_actual = Date.now();
+        if (tiempo_actual - parsed_json.fecha <= 60000) {
+          console.log("Datos desde Localstorage");
+          resolve(parsed_json.data);
+        } else {
+          fetchData()
+            .then((data) => {
+              const tiempo_inicio = Date.now();
+              const newData = { fecha: tiempo_inicio, data };
+              localStorage.setItem("FinishedMatch", JSON.stringify(newData));
+              resolve(data);
+            })
+            .catch((err) => reject(err));
+        }
+      } else {
+        fetchData()
+          .then((data) => {
+            const tiempo_inicio = Date.now();
+            const newData = { fecha: tiempo_inicio, data };
+            localStorage.setItem("FinishedMatch", JSON.stringify(newData));
+            resolve(data.response);
+          })
+          .catch((err) => reject(err));
+      }
+    });
+  }
+
+  function mostrarDatos() {
+    // Si ya existe una tabla creada, destruirla
+    if ($.fn.DataTable.isDataTable("#tablaResul")) {
+      $("#tablaResul").DataTable().clear().destroy();
+    }
+    const loader = document.querySelector(".loader");
+    loader.style.display = "block";
+    getData()
+      .then((data) => {
+        const partidos = document.getElementById("Partidos");
+        partidos.innerHTML = "";
+        data.forEach((element) => {
+          const fecha = new Date(element.fixture.date);
+          const fechaCorta = fecha.toLocaleDateString("es-CL");
+          const unixTime = element.fixture.timestamp;
+          const fechaHora = new Date(unixTime * 1000);
+          const hora = fechaHora.toLocaleTimeString();
+          const tr = document.createElement("tr");
+          tr.innerHTML = `
+          <td>${fechaCorta}</td>
+          <td>${hora}</td>
+          <td>${element.league.country}</td>
+          <td>${element.league.name}</td>
+          <td>${element.league.round}</td>
+          <td>${element.league.season}</td>
+          <td>
+            <img src="${
+              element.teams.home.logo
+            }" alt="Imagen del equipo local" class="centrada" />
+            ${element.teams.home.name}
+          </td>
+          <td>
+            <img src="${
+              element.teams.away.logo
+            }" alt="Imagen del equipo visitante" class="centrada" />
+            ${element.teams.away.name} 
+          </td>
+          <td>${
+            element.score.halftime.home !== null
+              ? element.score.halftime.home
+              : "-"
+          } : ${
+            element.score.halftime.away !== null
+              ? element.score.halftime.away
+              : "-"
+          }</td>
+          <td>${
+            element.score.fulltime.home !== null
+              ? element.score.fulltime.home
+              : "-"
+          } : ${
+            element.score.fulltime.away !== null
+              ? element.score.fulltime.away
+              : "-"
+          }</td>
+          <td><a href="pronosticos.html?fixture=${
+            element.fixture.id
+          }">Ver</a></td>
+          <td><a href="odds.html?fixture=${element.fixture.id}&home=${
+            element.teams.home.name
+          }&away=${element.teams.away.name}">Cuota</a></td>
+        `;
+          partidos.appendChild(tr);
+        });
+
+        $(document).ready(function () {
+          $("#tablaResul").DataTable({
+            responsive: true,
+            language: {
+              url: "//cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json",
+            },
+            bInfo: false,
+          });
+        });
+
+        loader.style.display = "none";
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  mostrarDatos();
+}
+
+function functionNext() {
+  url = `https://v3.football.api-sports.io/fixtures?date=${fecha}&status=NS&timezone=${timeZone}`;
+  function getData() {
+    return new Promise((resolve, reject) => {
+      const storedData = localStorage.getItem("NextMatch");
+
+      if (storedData) {
+        const parsed_json = JSON.parse(storedData);
+        const tiempo_actual = Date.now();
+        if (tiempo_actual - parsed_json.fecha <= 60000) {
+          console.log("Datos desde Localstorage");
+          resolve(parsed_json.data);
+        } else {
+          fetchData()
+            .then((data) => {
+              const tiempo_inicio = Date.now();
+              const newData = { fecha: tiempo_inicio, data };
+              localStorage.setItem("NextMatch", JSON.stringify(newData));
+              resolve(data);
+            })
+            .catch((err) => reject(err));
+        }
+      } else {
+        fetchData()
+          .then((data) => {
+            const tiempo_inicio = Date.now();
+            const newData = { fecha: tiempo_inicio, data };
+            localStorage.setItem("NextMatch", JSON.stringify(newData));
+            resolve(data.response);
+          })
+          .catch((err) => reject(err));
+      }
+    });
+  }
+
+  function mostrarDatos() {
+    // Si ya existe una tabla creada, destruirla
+    if ($.fn.DataTable.isDataTable("#tablaResul")) {
+      $("#tablaResul").DataTable().clear().destroy();
+    }
+    const loader = document.querySelector(".loader");
+    loader.style.display = "block";
+    getData()
+      .then((data) => {
+        const partidos = document.getElementById("Partidos");
+        partidos.innerHTML = "";
+        data.forEach((element) => {
+          const fecha = new Date(element.fixture.date);
+          const fechaCorta = fecha.toLocaleDateString("es-CL");
+          const unixTime = element.fixture.timestamp;
+          const fechaHora = new Date(unixTime * 1000);
+          const hora = fechaHora.toLocaleTimeString();
+          const tr = document.createElement("tr");
+          tr.innerHTML = `
+          <td>${fechaCorta}</td>
+          <td>${hora}</td>
+          <td>${element.league.country}</td>
+          <td>${element.league.name}</td>
+          <td>${element.league.round}</td>
+          <td>${element.league.season}</td>
+          <td>
+            <img src="${
+              element.teams.home.logo
+            }" alt="Imagen del equipo local" class="centrada" />
+            ${element.teams.home.name}
+          </td>
+          <td>
+            <img src="${
+              element.teams.away.logo
+            }" alt="Imagen del equipo visitante" class="centrada" />
+            ${element.teams.away.name} 
+          </td>
+          <td>${
+            element.score.halftime.home !== null
+              ? element.score.halftime.home
+              : "-"
+          } : ${
+            element.score.halftime.away !== null
+              ? element.score.halftime.away
+              : "-"
+          }</td>
+          <td>${
+            element.score.fulltime.home !== null
+              ? element.score.fulltime.home
+              : "-"
+          } : ${
+            element.score.fulltime.away !== null
+              ? element.score.fulltime.away
+              : "-"
+          }</td>
+          <td><a href="pronosticos.html?fixture=${
+            element.fixture.id
+          }">Ver</a></td>
+          <td><a href="odds.html?fixture=${element.fixture.id}&home=${
+            element.teams.home.name
+          }&away=${element.teams.away.name}">Cuota</a></td>
+        `;
+          partidos.appendChild(tr);
+        });
+
+        $(document).ready(function () {
+          $("#tablaResul").DataTable({
+            responsive: true,
+            language: {
+              url: "//cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json",
+            },
+            bInfo: false,
+          });
+        });
+
+        loader.style.display = "none";
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  mostrarDatos();
+}
+
+function fetchData() {
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "v3.football.api-sports.io",
+      "x-rapidapi-key": "22240ee711b7e2faa6b80fa55809e7db",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Datos desde la API:", data);
+      return data.response;
     });
 }
 
-mostrarDatos();
+functionAll();
