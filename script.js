ced10cc0a40b2a2a66bb1997d84d3028631ed06b
Claudio <claudiogonzalez0712@gmail.com>
2023-04-14 23:05:37 -0400

    Se le da tono oscuro a la pagina

--- script.js
+++ script.js
@@ -72,22 +72,22 @@ function functionAll() {
           const hora = fechaHora.toLocaleTimeString();
           const tr = document.createElement("tr");
           tr.innerHTML = `
-          <td>${fechaCorta}</td>
-          <td>${hora}</td>
           <td>${element.league.country}</td>
           <td>${element.league.name}</td>
           <td>${element.league.round}</td>
+          <td>${hora}</td>
+          <td>${fechaCorta}</td>
           <td>${element.league.season}</td>
-          <td>
+          <td class="text-nowrap">
             <img src="${
               element.teams.home.logo
-            }" alt="Imagen del equipo local" class="centrada" />
+            }" alt="Imagen del equipo local" class="ml-0 align-middle max-width-1rem"/>
             ${element.teams.home.name}
           </td>
-          <td>
+          <td class="text-nowrap">
             <img src="${
               element.teams.away.logo
-            }" alt="Imagen del equipo visitante" class="centrada" />
+            }" alt="Imagen del equipo visitante" class="ml-0 align-middle max-width-1rem"/>
             ${element.teams.away.name} 
           </td>
           <td>${
@@ -120,7 +120,17 @@ function functionAll() {
 
         $(document).ready(function () {
           $("#tablaResul").DataTable({
-            responsive: true,
+            responsive: {
+              details: {
+                  display: $.fn.dataTable.Responsive.display.modal({
+                    header: function(row) {
+                      var data = row.data();
+                      return data[0] + ' ' + data[1];
+                    }
+                  }),
+                  renderer: $.fn.dataTable.Responsive.renderer.tableAll()
+              }
+          },
             language: {
               url: "//cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json",
             },
@@ -180,75 +190,85 @@ function functionLive() {
     const loader = document.querySelector(".loader");
     loader.style.display = "block";
     getData()
-      .then((data) => {
-        const partidos = document.getElementById("Partidos");
-        partidos.innerHTML = "";
-        data.forEach((element) => {
-          const fecha = new Date(element.fixture.date);
-          const fechaCorta = fecha.toLocaleDateString("es-CL");
-          const unixTime = element.fixture.timestamp;
-          const fechaHora = new Date(unixTime * 1000);
-          const hora = fechaHora.toLocaleTimeString();
-          const tr = document.createElement("tr");
-          tr.innerHTML = `
-          <td>${fechaCorta}</td>
-          <td>${hora}</td>
-          <td>${element.league.country}</td>
-          <td>${element.league.name}</td>
-          <td>${element.league.round}</td>
-          <td>${element.league.season}</td>
-          <td>
-            <img src="${
-              element.teams.home.logo
-            }" alt="Imagen del equipo local" class="centrada" />
-            ${element.teams.home.name}
-          </td>
-          <td>
-            <img src="${
-              element.teams.away.logo
-            }" alt="Imagen del equipo visitante" class="centrada" />
-            ${element.teams.away.name} 
-          </td>
-          <td>${
-            element.score.halftime.home !== null
-              ? element.score.halftime.home
-              : "-"
-          } : ${
-            element.score.halftime.away !== null
-              ? element.score.halftime.away
-              : "-"
-          }</td>
-          <td>${
-            element.score.fulltime.home !== null
-              ? element.score.fulltime.home
-              : "-"
-          } : ${
-            element.score.fulltime.away !== null
-              ? element.score.fulltime.away
-              : "-"
-          }</td>
-          <td><a href="pronosticos.html?fixture=${
-            element.fixture.id
-          }">Ver</a></td>
-          <td><a href="odds.html?fixture=${element.fixture.id}&home=${
-            element.teams.home.name
-          }&away=${element.teams.away.name}">Cuota</a></td>
-        `;
-          partidos.appendChild(tr);
-        });
+    .then((data) => {
+      const partidos = document.getElementById("Partidos");
+      partidos.innerHTML = "";
+      data.forEach((element) => {
+        const fecha = new Date(element.fixture.date);
+        const fechaCorta = fecha.toLocaleDateString("es-CL");
+        const unixTime = element.fixture.timestamp;
+        const fechaHora = new Date(unixTime * 1000);
+        const hora = fechaHora.toLocaleTimeString();
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <td>${element.league.country}</td>
+        <td>${element.league.name}</td>
+        <td>${element.league.round}</td>
+        <td>${hora}</td>
+        <td>${fechaCorta}</td>
+        <td>${element.league.season}</td>
+        <td class="text-nowrap">
+          <img src="${
+            element.teams.home.logo
+          }" alt="Imagen del equipo local" class="ml-0 align-middle max-width-1rem"/>
+          ${element.teams.home.name}
+        </td>
+        <td class="text-nowrap">
+          <img src="${
+            element.teams.away.logo
+          }" alt="Imagen del equipo visitante" class="ml-0 align-middle max-width-1rem"/>
+          ${element.teams.away.name} 
+        </td>
+        <td>${
+          element.score.halftime.home !== null
+            ? element.score.halftime.home
+            : "-"
+        } : ${
+          element.score.halftime.away !== null
+            ? element.score.halftime.away
+            : "-"
+        }</td>
+        <td>${
+          element.score.fulltime.home !== null
+            ? element.score.fulltime.home
+            : "-"
+        } : ${
+          element.score.fulltime.away !== null
+            ? element.score.fulltime.away
+            : "-"
+        }</td>
+        <td><a href="pronosticos.html?fixture=${
+          element.fixture.id
+        }">Ver</a></td>
+        <td><a href="odds.html?fixture=${element.fixture.id}&home=${
+          element.teams.home.name
+        }&away=${element.teams.away.name}">Cuota</a></td>
+      `;
+        partidos.appendChild(tr);
+      });
 
-        $(document).ready(function () {
-          $("#tablaResul").DataTable({
-            responsive: true,
-            language: {
-              url: "//cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json",
-            },
-            bInfo: false,
-          });
+      $(document).ready(function () {
+        $("#tablaResul").DataTable({
+          responsive: {
+            details: {
+                display: $.fn.dataTable.Responsive.display.modal({
+                  header: function(row) {
+                    var data = row.data();
+                    return data[0] + ' ' + data[1];
+                  }
+                }),
+                renderer: $.fn.dataTable.Responsive.renderer.tableAll()
+            }
+        },
+          language: {
+            url: "//cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json",
+          },
+          bInfo: false,
         });
+      });
 
-        loader.style.display = "none";
-      })
+      loader.style.display = "none";
+    })
       .catch((err) => {
         console.log(err);
       });
@@ -299,75 +319,85 @@ function functionFinished() {
     const loader = document.querySelector(".loader");
     loader.style.display = "block";
     getData()
-      .then((data) => {
-        const partidos = document.getElementById("Partidos");
-        partidos.innerHTML = "";
-        data.forEach((element) => {
-          const fecha = new Date(element.fixture.date);
-          const fechaCorta = fecha.toLocaleDateString("es-CL");
-          const unixTime = element.fixture.timestamp;
-          const fechaHora = new Date(unixTime * 1000);
-          const hora = fechaHora.toLocaleTimeString();
-          const tr = document.createElement("tr");
-          tr.innerHTML = `
-          <td>${fechaCorta}</td>
-          <td>${hora}</td>
-          <td>${element.league.country}</td>
-          <td>${element.league.name}</td>
-          <td>${element.league.round}</td>
-          <td>${element.league.season}</td>
-          <td>
-            <img src="${
-              element.teams.home.logo
-            }" alt="Imagen del equipo local" class="centrada" />
-            ${element.teams.home.name}
-          </td>
-          <td>
-            <img src="${
-              element.teams.away.logo
-            }" alt="Imagen del equipo visitante" class="centrada" />
-            ${element.teams.away.name} 
-          </td>
-          <td>${
-            element.score.halftime.home !== null
-              ? element.score.halftime.home
-              : "-"
-          } : ${
-            element.score.halftime.away !== null
-              ? element.score.halftime.away
-              : "-"
-          }</td>
-          <td>${
-            element.score.fulltime.home !== null
-              ? element.score.fulltime.home
-              : "-"
-          } : ${
-            element.score.fulltime.away !== null
-              ? element.score.fulltime.away
-              : "-"
-          }</td>
-          <td><a href="pronosticos.html?fixture=${
-            element.fixture.id
-          }">Ver</a></td>
-          <td><a href="odds.html?fixture=${element.fixture.id}&home=${
-            element.teams.home.name
-          }&away=${element.teams.away.name}">Cuota</a></td>
-        `;
-          partidos.appendChild(tr);
-        });
+    .then((data) => {
+      const partidos = document.getElementById("Partidos");
+      partidos.innerHTML = "";
+      data.forEach((element) => {
+        const fecha = new Date(element.fixture.date);
+        const fechaCorta = fecha.toLocaleDateString("es-CL");
+        const unixTime = element.fixture.timestamp;
+        const fechaHora = new Date(unixTime * 1000);
+        const hora = fechaHora.toLocaleTimeString();
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <td>${element.league.country}</td>
+        <td>${element.league.name}</td>
+        <td>${element.league.round}</td>
+        <td>${hora}</td>
+        <td>${fechaCorta}</td>
+        <td>${element.league.season}</td>
+        <td class="text-nowrap">
+          <img src="${
+            element.teams.home.logo
+          }" alt="Imagen del equipo local" class="ml-0 align-middle max-width-1rem"/>
+          ${element.teams.home.name}
+        </td>
+        <td class="text-nowrap">
+          <img src="${
+            element.teams.away.logo
+          }" alt="Imagen del equipo visitante" class="ml-0 align-middle max-width-1rem"/>
+          ${element.teams.away.name} 
+        </td>
+        <td>${
+          element.score.halftime.home !== null
+            ? element.score.halftime.home
+            : "-"
+        } : ${
+          element.score.halftime.away !== null
+            ? element.score.halftime.away
+            : "-"
+        }</td>
+        <td>${
+          element.score.fulltime.home !== null
+            ? element.score.fulltime.home
+            : "-"
+        } : ${
+          element.score.fulltime.away !== null
+            ? element.score.fulltime.away
+            : "-"
+        }</td>
+        <td><a href="pronosticos.html?fixture=${
+          element.fixture.id
+        }">Ver</a></td>
+        <td><a href="odds.html?fixture=${element.fixture.id}&home=${
+          element.teams.home.name
+        }&away=${element.teams.away.name}">Cuota</a></td>
+      `;
+        partidos.appendChild(tr);
+      });
 
-        $(document).ready(function () {
-          $("#tablaResul").DataTable({
-            responsive: true,
-            language: {
-              url: "//cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json",
-            },
-            bInfo: false,
-          });
+      $(document).ready(function () {
+        $("#tablaResul").DataTable({
+          responsive: {
+            details: {
+                display: $.fn.dataTable.Responsive.display.modal({
+                  header: function(row) {
+                    var data = row.data();
+                    return data[0] + ' ' + data[1];
+                  }
+                }),
+                renderer: $.fn.dataTable.Responsive.renderer.tableAll()
+            }
+        },
+          language: {
+            url: "//cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json",
+          },
+          bInfo: false,
         });
+      });
 
-        loader.style.display = "none";
-      })
+      loader.style.display = "none";
+    })
       .catch((err) => {
         console.log(err);
       });
@@ -418,75 +448,85 @@ function functionNext() {
     const loader = document.querySelector(".loader");
     loader.style.display = "block";
     getData()
-      .then((data) => {
-        const partidos = document.getElementById("Partidos");
-        partidos.innerHTML = "";
-        data.forEach((element) => {
-          const fecha = new Date(element.fixture.date);
-          const fechaCorta = fecha.toLocaleDateString("es-CL");
-          const unixTime = element.fixture.timestamp;
-          const fechaHora = new Date(unixTime * 1000);
-          const hora = fechaHora.toLocaleTimeString();
-          const tr = document.createElement("tr");
-          tr.innerHTML = `
-          <td>${fechaCorta}</td>
-          <td>${hora}</td>
-          <td>${element.league.country}</td>
-          <td>${element.league.name}</td>
-          <td>${element.league.round}</td>
-          <td>${element.league.season}</td>
-          <td>
-            <img src="${
-              element.teams.home.logo
-            }" alt="Imagen del equipo local" class="centrada" />
-            ${element.teams.home.name}
-          </td>
-          <td>
-            <img src="${
-              element.teams.away.logo
-            }" alt="Imagen del equipo visitante" class="centrada" />
-            ${element.teams.away.name} 
-          </td>
-          <td>${
-            element.score.halftime.home !== null
-              ? element.score.halftime.home
-              : "-"
-          } : ${
-            element.score.halftime.away !== null
-              ? element.score.halftime.away
-              : "-"
-          }</td>
-          <td>${
-            element.score.fulltime.home !== null
-              ? element.score.fulltime.home
-              : "-"
-          } : ${
-            element.score.fulltime.away !== null
-              ? element.score.fulltime.away
-              : "-"
-          }</td>
-          <td><a href="pronosticos.html?fixture=${
-            element.fixture.id
-          }">Ver</a></td>
-          <td><a href="odds.html?fixture=${element.fixture.id}&home=${
-            element.teams.home.name
-          }&away=${element.teams.away.name}">Cuota</a></td>
-        `;
-          partidos.appendChild(tr);
-        });
+    .then((data) => {
+      const partidos = document.getElementById("Partidos");
+      partidos.innerHTML = "";
+      data.forEach((element) => {
+        const fecha = new Date(element.fixture.date);
+        const fechaCorta = fecha.toLocaleDateString("es-CL");
+        const unixTime = element.fixture.timestamp;
+        const fechaHora = new Date(unixTime * 1000);
+        const hora = fechaHora.toLocaleTimeString();
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <td>${element.league.country}</td>
+        <td>${element.league.name}</td>
+        <td>${element.league.round}</td>
+        <td>${hora}</td>
+        <td>${fechaCorta}</td>
+        <td>${element.league.season}</td>
+        <td class="text-nowrap">
+          <img src="${
+            element.teams.home.logo
+          }" alt="Imagen del equipo local" class="ml-0 align-middle max-width-1rem"/>
+          ${element.teams.home.name}
+        </td>
+        <td class="text-nowrap">
+          <img src="${
+            element.teams.away.logo
+          }" alt="Imagen del equipo visitante" class="ml-0 align-middle max-width-1rem"/>
+          ${element.teams.away.name} 
+        </td>
+        <td>${
+          element.score.halftime.home !== null
+            ? element.score.halftime.home
+            : "-"
+        } : ${
+          element.score.halftime.away !== null
+            ? element.score.halftime.away
+            : "-"
+        }</td>
+        <td>${
+          element.score.fulltime.home !== null
+            ? element.score.fulltime.home
+            : "-"
+        } : ${
+          element.score.fulltime.away !== null
+            ? element.score.fulltime.away
+            : "-"
+        }</td>
+        <td><a href="pronosticos.html?fixture=${
+          element.fixture.id
+        }">Ver</a></td>
+        <td><a href="odds.html?fixture=${element.fixture.id}&home=${
+          element.teams.home.name
+        }&away=${element.teams.away.name}">Cuota</a></td>
+      `;
+        partidos.appendChild(tr);
+      });
 
-        $(document).ready(function () {
-          $("#tablaResul").DataTable({
-            responsive: true,
-            language: {
-              url: "//cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json",
-            },
-            bInfo: false,
-          });
+      $(document).ready(function () {
+        $("#tablaResul").DataTable({
+          responsive: {
+            details: {
+                display: $.fn.dataTable.Responsive.display.modal({
+                  header: function(row) {
+                    var data = row.data();
+                    return data[0] + ' ' + data[1];
+                  }
+                }),
+                renderer: $.fn.dataTable.Responsive.renderer.tableAll()
+            }
+        },
+          language: {
+            url: "//cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json",
+          },
+          bInfo: false,
         });
+      });
 
-        loader.style.display = "none";
-      })
+      loader.style.display = "none";
+    })
       .catch((err) => {
         console.log(err);
       });
